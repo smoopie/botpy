ae63529693bac849275827746d85ee11b00b7355
smoopie <38010419+smoopie@users.noreply.github.com>
2021-08-29 00:04:01 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,4 +18,4 @@ async def on_member_remove(member):
 async def ping(ctx):
     await ctx.send(f"Hey! I am up and running with a ping of {round(client.latency*1000)}ms.")
 
-client.run('')   #Place token between the ''
+client.run('ODgxMjk1NjEwNTc2MjUyOTI4.YSqwfw.H9WnZSFhXJP7JEcoPBfZi0xs6Ic')   #Place token between the ''
